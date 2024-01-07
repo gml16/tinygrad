9e9d04f6b6ab8b6e690103fa255a2d78e569f6dd
Guy Leroy <g.m.leroy@outlook.com>
2024-01-07 23:20:14 +0000

    import test debug 2

--- extra/optimization/test_debug2.py
+++ extra/optimization/test_debug2.py
@@ -1,5 +1,6 @@
 
 from tinygrad import Tensor
+<<<<<<< Updated upstream
 
 a = Tensor.randn(10, requires_grad=True)
 loss = (a*(a ^ 0)).sum().backward()
@@ -9,3 +10,29 @@ import torch
 a = torch.autograd.Variable(torch.Tensor(a.numpy()), requires_grad=True)
 loss = (a*(a ^ 0)).sum().backward()
 print(a.grad)
+=======
+import torch
+
+# t1 = torch.ones(4, requires_grad=True)
+# t2 = torch.ones(4, requires_grad=True)
+# loss = (t1 == t2).sum().backward()
+# print(t1.grad)
+
+t = Tensor.ones(4, requires_grad=True)
+# loss = (t*(t < 10)).sum().backward()
+# print(t.grad.numpy())
+
+a = torch.autograd.Variable(torch.Tensor(t.numpy()), requires_grad=True)
+# loss = (a*(a < 0)).sum().backward()
+# print(a.grad.numpy())
+
+b = torch.autograd.Variable(torch.Tensor(t.numpy()), requires_grad=True)
+# loss = (a*(a < 0)).sum().backward()
+# print(a.grad.numpy())
+
+# tt1 = Tensor.ones(4, requires_grad=True)
+# tt2 = Tensor.ones(4, requires_grad=True)
+# tt1 = torch.ones(4, requires_grad=True)
+# tt2 = torch.ones(4, requires_grad=True)
+# (tt1 == tt2).sum().backward()
+>>>>>>> Stashed changes
